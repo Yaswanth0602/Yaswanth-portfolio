3f6dd109d08dde7a9a7644f7b580d5e7cfac17f1
Yaswanth0602 <yaswanthreddy0602@gmail.com>
2025-03-21 22:24:49 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show alert when button is clicked
+function showMessage() {
+    alert("Hello! Welcome to my portfolio.");
+}
+
+// Change background color when clicking a button
+function changeBgColor() {
+    document.body.style.backgroundColor = "#d4f1f4";
+}
